7c6f1607ee44799d173bddebd2f703be59df4b77
HASEGAWA Tomoki <tomoki@dgcircus.com>
2015-03-08 02:07:05 +0900

    Initial Commit

---
+++ GourmetSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GourmetSearch
+//
+//  Created by 長谷川 智希 on 2015/03/08.
+//  Copyright (c) 2015年 Digital Circus Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ GourmetSearchTests/GourmetSearchTests.swift
@@ -0,0 +1,36 @@
+//
+//  GourmetSearchTests.swift
+//  GourmetSearchTests
+//
+//  Created by 長谷川 智希 on 2015/03/08.
+//  Copyright (c) 2015年 Digital Circus Inc. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GourmetSearchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
