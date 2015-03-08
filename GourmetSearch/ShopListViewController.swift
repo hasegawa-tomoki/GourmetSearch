1c95f58acf4b245abf16113a9bf35d97997cb748
HASEGAWA Tomoki <tomoki@dgcircus.com>
2015-03-08 17:32:01 +0900

    Chapter 6-4

--- GourmetSearch/ShopListViewController.swift
+++ GourmetSearch/ShopListViewController.swift
@@ -1,20 +1,65 @@
 import UIKit
 
 class ShopListViewController: UIViewController,
-	UITableViewDelegate, UITableViewDataSource {
+UITableViewDelegate, UITableViewDataSource {
 	
 	@IBOutlet weak var tableView: UITableView!
-
+	
+	var yls: YahooLocalSearch = YahooLocalSearch()
+	var loadDataObserver: NSObjectProtocol?
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 	}
-
+	
+	override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		var qc = QueryCondition()
+		qc.query = "ハンバーガー"
+		
+		yls = YahooLocalSearch(condition: qc)
+		
+		// 読込完了通知を受信した時の処理
+		loadDataObserver = NSNotificationCenter.defaultCenter().addObserverForName(
+			yls.YLSLoadCompleteNotification,
+			object: nil,
+			queue: nil,
+			usingBlock: {
+				(notification) in
+				
+				// エラーがあればダイアログを開く
+				if notification.userInfo != nil {
+					if let userInfo = notification.userInfo as? [String: String!] {
+						if userInfo["error"] != nil {
+							let alertView = UIAlertController(title: "通信エラー",
+								message: "通信エラーが発生しました。",
+								preferredStyle: .Alert)
+							alertView.addAction(
+								UIAlertAction(title: "OK", style: .Default) {
+									action in return
+								}
+							)
+							self.presentViewController(alertView,
+								animated: true, completion: nil)
+						}
+					}
+				}
+			}
+		)
+		
+		yls.loadData(reset: true)
+	}
+	
+	override func viewWillDisappear(animated: Bool) {
+		// 通知の監視を終了
+		NSNotificationCenter.defaultCenter().removeObserver(self.loadDataObserver!)
+	}
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
-
+	
 	// MARK: - UITableViewDelegate
 	func tableView(tableView: UITableView,
 		heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
---
+++ GourmetSearch/YahooLocal.swift
@@ -0,0 +1,228 @@
+import Foundation
+import Alamofire
+import SwiftyJSON
+import AlamofireSwiftyJSON
+
+public struct Shop: Printable {
+	public var gid: String? = nil
+	public var name: String? = nil
+	public var photoUrl: String? = nil
+	public var yomi: String? = nil
+	public var tel: String? = nil
+	public var address: String? = nil
+	public var lat: Double? = nil
+	public var lon: Double? = nil
+	public var catchCopy: String? = nil
+	public var hasCoupon = false
+	public var station: String? = nil
+	public var description: String {
+		get {
+			var string = "\nGid: \(gid)\n"
+			string += "Name: \(name)\n"
+			string += "PhotoUrl: \(photoUrl)\n"
+			string += "Yomi: \(yomi)\n"
+			string += "Tel: \(tel)\n"
+			string += "Address: \(address)\n"
+			string += "Lat & Lon: (\(lat), \(lon))\n"
+			string += "CatchCopy: \(catchCopy)\n"
+			string += "hasCoupon: \(hasCoupon)\n"
+			string += "Station: \(station)\n"
+			return string
+		}
+	}
+}
+
+public struct QueryCondition {
+	// キーワード
+	public var query: String? = nil
+	// 店舗ID
+	public var gid: String? = nil
+	// ソート順
+	public enum Sort: String {
+		case Score = "score"
+		case Geo = "geo"
+	}
+	public var sort: Sort = .Score
+	// 緯度
+	public var lat: Double? = nil
+	// 経度
+	public var lon: Double? = nil
+	// 距離
+	public var dist: Double? = nil
+	// 検索パラメタディクショナリ
+	public var queryParams: [String: String] {
+		get {
+			var params = [String: String]()
+			// キーワード
+			if let unwrapped = query {
+				params["query"] = unwrapped
+			}
+			// 店舗ID
+			if let unwrapped = gid {
+				params["gid"] = unwrapped
+			}
+			// ソート順
+			switch sort {
+			case .Score:
+				params["sort"] = "score"
+			case .Geo:
+				params["sort"] = "geo"
+			}
+			// 緯度
+			if let unwrapped = lat {
+				params["lat"] = "\(unwrapped)"
+			}
+			// 経度
+			if let unwrapped = lon {
+				params["lon"] = "\(unwrapped)"
+			}
+			// 距離
+			if let unwrapped = dist {
+				params["dist"] = "\(unwrapped)"
+			}
+			// デバイス: mobile固定
+			params["device"] = "mobile"
+			// グルーピング: gid固定
+			params["group"] = "gid"
+			// 画像があるデータのみ検索する: true固定
+			params["image"] = "true"
+			// 業種コード: 01（グルメ）固定
+			params["gc"] = "01"
+			return params
+		}
+	}
+}
+
+public class YahooLocalSearch {
+	// 読込開始Notification
+	public let YLSLoadStartNotification = "YLSLoadStartNotification"
+	// 読込完了Notification
+	public let YLSLoadCompleteNotification = "YLSLoadCompleteNotification"
+	
+	// Yahoo!ローカルサーチAPIのアプリケーションID
+	let apiId = "（アプリケーションID）"
+	// APIのベースURL
+	let apiUrl = "http://search.olp.yahooapis.jp/OpenLocalPlatform/V1/localSearch"
+	// 1ページのレコード数
+	let perPage = 10
+	// 読込済の店舗
+	public var shops = [Shop]()
+	// 全何件か
+	public var total = 0
+	// 検索条件
+	var condition: QueryCondition = QueryCondition() {
+		// プロパティオブザーバ: 新しい値がセットされた後に読込済の店舗を捨てる
+		didSet {
+			shops = []
+			total = 0
+		}
+	}
+	// パラメタ無しのイニシャライザ
+	public init(){}
+	// 検索条件をパラメタとして持つイニシャライザ
+	public init(condition: QueryCondition){ self.condition = condition }
+	// APIからデータを読み込む。
+	// reset = trueならデータを捨てて最初から読み込む
+	public func loadData(reset: Bool = false) {
+		// reset = true なら今までの結果を捨てる
+		if reset {
+			shops = []
+			total = 0
+		}
+		// 条件ディクショナリを取得
+		var params = condition.queryParams
+		// 検索条件以外のAPIパラメタを設定
+		params["appid"] = apiId
+		params["output"] = "json"
+		params["start"] = String(shops.count + 1)
+		params["results"] = String(perPage)
+		
+		// API実行開始を通知する
+		NSNotificationCenter.defaultCenter().postNotificationName(
+			YLSLoadStartNotification, object: nil)
+		
+		// APIリクエスト実行
+		Alamofire.request(.GET, apiUrl, parameters: params).responseSwiftyJSON {
+			// リクエストが完了した時に実行されるクロージャ
+			(request, response, json, error) -> Void in
+			
+			// エラーがあれば終了
+			if error != nil {
+				// API実行終了を通知する
+				var message = "Unknown error."
+				if let description = error?.description {
+					message = description
+				}
+				NSNotificationCenter.defaultCenter().postNotificationName(
+					self.YLSLoadCompleteNotification,
+					object: nil,
+					userInfo: ["error": message])
+				return
+			}
+			
+			// 店舗データをself.shopsに追加する
+			for (key, item) in json["Feature"] {
+				var shop = Shop()
+				// 店舗ID
+				shop.gid = item["Gid"].string
+				// 店舗名
+				var name = item["Name"].string
+				// ' が &#39; という形でエンコードされているのでデコードする
+				shop.name = name?.stringByReplacingOccurrencesOfString("&#39;",
+					withString: "'",
+					options: .LiteralSearch,
+					range: nil)
+				// 読み
+				shop.yomi = item["Property"]["Yomi"].string
+				// 電話
+				shop.tel = item["Property"]["Tel1"].string
+				// 住所
+				shop.address = item["Property"]["Address"].string
+				// 緯度＆経度
+				if let geometry = item["Geometry"]["Coordinates"].string {
+					let components = geometry.componentsSeparatedByString(",")
+					// 緯度
+					shop.lat = (components[1] as NSString).doubleValue
+					// 経度
+					shop.lon = (components[0] as NSString).doubleValue
+				}
+				// キャッチコピー
+				shop.catchCopy = item["Property"]["CatchCopy"].string
+				// 店舗写真
+				shop.photoUrl = item["Property"]["LeadImage"].string
+				// クーポン有無
+				if item["Property"]["CouponFlag"].string == "true" {
+					shop.hasCoupon = true
+				}
+				// 駅
+				if let stations = item["Property"]["Station"].array {
+					// 路線名は「/」で結合されているので最初の1つを使う
+					var line = ""
+					if let lineString = stations[0]["Railway"].string {
+						let lines = lineString.componentsSeparatedByString("/")
+						line = lines[0]
+					}
+					if let station = stations[0]["Name"].string {
+						// 駅名と路線名があれば両方入れる。
+						shop.station = "\(line) \(station)"
+					} else {
+						// 駅名が無い場合路線名のみ入れる。
+						shop.station = "\(line)"
+					}
+				}
+				
+				self.shops.append(shop)
+			}
+			// 総件数を反映
+			if let total = json["ResultInfo"]["Total"].int {
+				self.total = total
+			} else {
+				self.total = 0
+			}
+			
+			// API実行終了を通知する
+			NSNotificationCenter.defaultCenter().postNotificationName(
+				self.YLSLoadCompleteNotification, object: nil)
+		}
+	}
+}
